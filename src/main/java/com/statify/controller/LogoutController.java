f3d5ca7b4e277e72e113941bb772910ff1ff288c
ExeRed <valentinbibik04@gmail.com>
2024-03-25 18:25:16 +0100

    HTML Controller

--- src/main/java/com/statify/controller/LogoutController.java
+++ src/main/java/com/statify/controller/LogoutController.java
@@ -4,6 +4,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -11,15 +13,13 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class LogoutController {
 
-    @GetMapping("/logout")
+    @PostMapping("/logout")
     public String logout(HttpServletRequest request, HttpServletResponse response, HttpSession session, Authentication authentication) {
-        // Удаление токена доступа или других данных аутентификации, если это необходимо
         if (authentication instanceof OAuth2Authentication) {
             session.removeAttribute("token");
         }
-        session.invalidate(); // Очистка сессии
+        session.invalidate();
 
-        // Перенаправление на страницу выхода Spotify
-        return "redirect:https://www.spotify.com/logout/";
+        return "home";
     }
 }
